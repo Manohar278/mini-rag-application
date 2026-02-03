cbaea8adc32a97dffc10d482fd68fce89bd5e2cb
Manohar278 <Anapurammanohargoud@gmail.com>
2026-02-03 22:50:29 +0530

    Refactor Pinecone initialization comment in ingest_pdf

--- ingest.py
+++ ingest.py
@@ -5,8 +5,6 @@ from sentence_transformers import SentenceTransformer
 embed_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def ingest_pdf(pdf_path):
-    # Pinecone initialized INSIDE the function
     pc = Pinecone(api_key=os.environ["pcsk_7N8TDx_T7CDM66rqR9VvwPRVHfCZd1iJejAdNkA1VStAXXRCcnZeRBcWMibb5pHyXCBfSk"])
     index = pc.Index("mini-rag")
-
-    # your existing ingestion logic continues here
+    # continue ingestion logic
