8837a76e645f962cfa97d9b788e9ce666a82561a
Manohar278 <Anapurammanohargoud@gmail.com>
2026-02-04 00:22:56 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,13 +1,14 @@
 from sentence_transformers import SentenceTransformer, CrossEncoder
 from transformers import pipeline
-from pinecone_utils import index
+from pinecone_utils import get_index
 
-# Models
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 llm = pipeline("text-generation", model="distilgpt2")
 
 def retrieve(query, top_k=5):
+    index = get_index()  # ✅ called only when needed
+
     query_vector = embedding_model.encode(query).tolist()
 
     results = index.query(
